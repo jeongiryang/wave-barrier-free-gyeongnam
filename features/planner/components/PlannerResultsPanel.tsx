1776fb9e844225b30b249732297c728b9f025463
정이량 <eryang1118@gmail.com>
2026-08-28 23:44:13 +0900

    refactor(planner): 경로와 출처 정보를 통합 여정 결과로 구성

---
+++ features/planner/components/PlannerResultsPanel.tsx
@@ -0,0 +1,23 @@
+import type { useAudioGuide } from "../hooks/useAudioGuide";
+import type { usePlannerParticipation } from "../hooks/usePlannerParticipation";
+import type { ApiStatus, PlanData } from "../types";
+import PlannerEvidencePanel from "./PlannerEvidencePanel";
+import PlannerRouteOverview from "./PlannerRouteOverview";
+
+interface PlannerResultsPanelProps {
+  plan: PlanData | null;
+  region: string;
+  theme: string;
+  selectedProfileIds: string[];
+  statuses: ApiStatus[];
+  liveCount: number;
+  audioGuide: ReturnType<typeof useAudioGuide>;
+  participation: ReturnType<typeof usePlannerParticipation>;
+}
+
+export default function PlannerResultsPanel(props: PlannerResultsPanelProps) {
+  return <div className="journey-results">
+    <PlannerRouteOverview {...props} />
+    <PlannerEvidencePanel plan={props.plan} statuses={props.statuses} />
+  </div>;
+}
